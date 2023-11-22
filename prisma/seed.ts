1bc600c26ea3f1880f87ff2f290c32c978a20db6
FerArteaga <oarteaga@applaudostudios.com>
2023-11-22 09:19:50 -0600

    generate roles and products, create seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,9 @@
-// prisma/seed.ts
-
 import { PrismaClient } from '@prisma/client';
 
-// initialize Prisma Client
 const prisma = new PrismaClient();
 
 async function main() {
-  // create two dummy articles
-  const user1 = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { email: 'fermerinonew@gmail.com' },
     update: {},
     create: {
@@ -15,15 +11,50 @@ async function main() {
     },
   });
 
-  const user2 = await prisma.user.upsert({
-    where: { email: 'fernand0scar@hotmai.com' },
+  await prisma.user.upsert({
+    where: { email: 'fernand0scar@hotmail.com' },
     update: {},
     create: {
       email: 'fernand0scar@hotmail.com',
     },
   });
 
-  console.log({ user1, user2 });
+  await prisma.role.upsert({
+    where: { name: 'Manager' },
+    update: {},
+    create: {
+      name: 'Manager',
+    },
+  });
+  await prisma.role.upsert({
+    where: { name: 'Client' },
+    update: {},
+    create: {
+      name: 'Client',
+    },
+  });
+
+  await prisma.product.upsert({
+    where: { name: 'shampoo goof goof' },
+    update: {},
+    create: {
+      name: 'shampoo goof goof',
+      description: 'is an incredible shampoo',
+      price: 12.34,
+      stock: 4,
+    },
+  });
+
+  await prisma.product.upsert({
+    where: { name: 'shampoo meow meow' },
+    update: {},
+    create: {
+      name: 'shampoo meow meow',
+      description: 'is an incredible shampoo',
+      price: 11.34,
+      stock: 5,
+    },
+  });
 }
 
 // execute the main function
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ProductsModule, UsersModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
